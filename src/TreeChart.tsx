d7deb990ade1e58f6b8ecfc3052f90923f162645
Kiran PS <pskirann@gmail.com>
2019-02-24 21:26:59 +0530

    conver TreeChart to stateless component

--- src/TreeChart.tsx
+++ src/TreeChart.tsx
@@ -1,50 +1,34 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import * as d3 from "d3";
 import { diagonal } from "./helper";
 import { SVG, Text, Circle, Path, Translate } from "./Styled";
+import { TreeChartProps, PointNode } from "./types";
 
-interface Node {
-  value: string;
-  children: Node[];
-}
+const TreeChart = ({ data, parsed }: TreeChartProps) => {
+  const root = d3.tree().size([320, 200])(d3.hierarchy(data));
+  const lines = root.links().map(diagonal);
+  const circles = root.descendants().map((x: PointNode) => ({
+    ...x,
+    active: parsed.findIndex(d => d.x === x.x && d.y === x.y) >= 0
+  }));
 
-interface NodeParsed {
-  x: Float32Array;
-  y: Float32Array;
-  value: number;
-}
-interface TreeChartProps {
-  data: Node;
-  parsed: NodeParsed[];
-}
+  return (
+    <SVG>
+      <Translate>
+        {lines.map((x: any, i: number) => (
+          <Path key={i} d={x} />
+        ))}
+        {circles.map((x: any, i: number) => (
+          <React.Fragment key={i}>
+            <Circle cx={x.x} cy={x.y} active={x.active} />
+            <Text dx={x.x - 7} dy={x.y + 4} active={x.active}>
+              {x.value}
+            </Text>
+          </React.Fragment>
+        ))}
+      </Translate>
+    </SVG>
+  );
+};
 
-export default class TreeChart extends PureComponent<TreeChartProps> {
-  root: any = d3.tree().size([320, 200])(d3.hierarchy(this.props.data));
-
-  render() {
-    const { parsed } = this.props;
-    const lines = this.root.links().map(diagonal);
-    const circles = this.root.descendants().map((x: any) => ({
-      ...x,
-      active: parsed.findIndex(d => d.x === x.x && d.y === x.y) >= 0
-    }));
-
-    return (
-      <SVG>
-        <Translate>
-          {lines.map((x: any, i: number) => (
-            <Path key={i} d={x} />
-          ))}
-          {circles.map((x: any, i: number) => (
-            <React.Fragment key={i}>
-              <Circle cx={x.x} cy={x.y} active={x.active} />
-              <Text dx={x.x - 7} dy={x.y + 4} active={x.active}>
-                {x.value}
-              </Text>
-            </React.Fragment>
-          ))}
-        </Translate>
-      </SVG>
-    );
-  }
-}
+export default TreeChart;
